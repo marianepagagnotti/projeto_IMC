e6619e3becac9c4da5a9d274cf416e610701803b
Usuario <Usuario@CPD02-M19>
2022-03-23 15:24:05 -0300

    Adicionar arquivos de projeto.

---
+++ IMC_2303/IMC_2303.UWP/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace IMC_2303.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new IMC_2303.App());
+        }
+    }
+}
---
+++ IMC_2303/IMC_2303/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IMC_2303
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            string name = txt_nome.Text;
+            double peso = double.Parse(txt_peso.Text);
+            double altura = double.Parse(txt_altura.Text);
+
+            double conta = peso / (altura * altura);
+
+           if (conta <= 18.5)
+            {
+                DisplayAlert("VOCÊ ESTÁ ABAIXO DO PESO","Seu imc é de "+conta.ToString("0.0"), "OK");
+            
+            }else if (18.5 < conta && conta <=24.9) {
+                
+                DisplayAlert("VOCÊ ESTÁ NO PESO NORMAL", "Seu imc é de "+conta.ToString("0.0"), "OK");
+
+            }else if (25 <= conta && conta <= 29.9)
+            {
+                DisplayAlert("VOCÊ ESTÁ ACIMA DO PESO", "Seu imc é de "+conta.ToString("0.0"), "OK");
+            
+            }else if (30 <= conta && conta <= 34.9)
+            {
+                DisplayAlert("VOCÊ ESTÁ NA OBESIDADE GRAU 1", "Seu imc é de "+conta.ToString("0.0"), "OK");
+            
+            }else if (35 <= conta && conta <= 39.9)
+            {
+                DisplayAlert("VOCÊ ESTÁ NA OBESIDADE GRAU 2", "Seu imc é de "+conta.ToString("0.0"), "OK");
+            
+            }else if (conta>= 40)
+            {
+                DisplayAlert("VOCÊ ESTÁ NA OBESIDADE GRAU 3", "Seu imc é de "+conta.ToString("0.0"), "OK");
+            }
+
+            
+            
+
+
+
+
+
+        }
+    }
+}
